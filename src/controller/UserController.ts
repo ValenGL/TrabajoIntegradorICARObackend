ee96320bdea2b424eba70388488e1c589ed3ca76
Valentin <valentingonzalezlou@gmail.com>
2021-08-03 22:43:10 -0300

    Trabajando en los metodos y en los campos de la bd

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,26 +1,100 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from "typeorm";
+import { Request, Response } from "express";
+import { User } from "../entity/User";
+import { validate } from "class-validator";
 
 export class UserController {
+  static getAll = async (req: Request, res: Response) => {
+    const userRepository = getRepository(User);
+    try {
+      const users = await userRepository.find();
+    } catch (e) {
+      res.status(400).json({ menssage: "Something goes wrong" });
+    }
+  };
 
-    private userRepository = getRepository(User);
+  static getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const userRepository = getRepository(User);
+    try {
+      const user = await userRepository.findOneOrFail(id);
+      res.send(User);
+    } catch (e) {
+      res.status(404).json({ message: "Not result" });
+    }
+  };
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find();
+  static newUser = async (req: Request, res: Response) => {
+    const { username, password, role } = req.body;
+    const user = new User();
+
+    user.username = username;
+    user.password = password;
+    user.role = role;
+
+    // Validate
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(errors);
     }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOne(request.params.id);
+    // TO DO: HASH PASSWORD
+
+    const userRepository = getRepository(User);
+    try {
+      await userRepository.save(user);
+    } catch (e) {
+      return res.status(409).json({ message: "Username already exists" });
+    }
+    // All ok
+    res.send("User created");
+  };
+
+  static editUser = async (req: Request, res: Response) => {
+    let user;
+    const { id } = req.params;
+    const { username, role } = req.body;
+
+    const userRepository = getRepository(User);
+    // Try get user
+    try {
+      user = await userRepository.findOneOrFail(id);
+      user.username = username;
+      user.role = role;
+    } catch (e) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body);
+    const errors = await validate(user);
+    if (errors.length > 0) {
+      return res.status(400).json(errors);
     }
 
-    async remove(request: Request, response: Response, next: NextFunction) {
-        let userToRemove = await this.userRepository.findOne(request.params.id);
-        await this.userRepository.remove(userToRemove);
+    // Try to save user
+
+    try {
+      await userRepository.save(user);
+    } catch (e) {
+      return res.status(409).json({ message: "Username already in use" });
+    }
+    res.status(201).json({ message: "User update" });
+  };
+
+  static deleteUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const userRepository = getRepository(User);
+    let user: User;
+
+    try {
+      user = await userRepository.findOneOrFail(id);
+    } catch (e) {
+      return res.status(404).json({ message: " User not found" });
     }
 
+    // Remove the user
+    userRepository.delete(id);
+    res.status(201).json({ message: "User deleted" });
+  };
 }
+
+export default UserController;
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,38 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { MinLength, IsNotEmpty } from "class-validator";
 
 @Entity()
+@Unique(["username"])
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  @MinLength(6)
+  username: string;
 
-    @Column()
-    firstName: string;
+  @Column()
+  @MinLength(8)
+  password: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  @IsNotEmpty()
+  role: string;
 
-    @Column()
-    age: number;
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
 
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  // TO DO resto de los campos
 }
